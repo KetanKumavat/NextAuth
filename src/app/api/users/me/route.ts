4885346ccb523a5815684acae7887bf4e33a7388
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2023-12-31 17:18:42 +0530

    profile page done

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,7 +8,8 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDataFromToken(request);
-    const user = await User.findOne({_id:userId}).select("-password");
+    const userName = await getDataFromToken(request);
+    const user = await User.findById(userId);
     return NextResponse.json({ message: "User Found", data: user });
   } catch (error) {
     return NextResponse.json({ error: error.message });
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Link from "next/link";
@@ -9,6 +9,7 @@ export default function ProfilePage() {
     const router = useRouter();
     const [data,setData] = React.useState("Nothing Here");
     const [username, setUsername] = React.useState("Username");
+    const [email,setEmail] = React.useState("User's Email");
     const logout = async () => {
         try {
             await axios.get("/api/users/logout");
@@ -22,9 +23,14 @@ export default function ProfilePage() {
     const getUserDetails = async () => {
       const res = await axios.get('/api/users/me');
       console.log(res.data);
-      setData(res.data.data._id)
-      setUsername(res.data.data.user)
+      setData(res.data.data._id);
+      setUsername(res.data.data.username)
+      setEmail(res.data.data.email);
     }
+    useEffect(() => {
+      getUserDetails();
+    });
+
     return (
       <div className="Home">
         <main className="flex min-h-screen flex-col items-center overflow-hidden justify-between">
@@ -57,12 +63,18 @@ export default function ProfilePage() {
           <div className="flex justify-center flex-col items-center h-screen w-full mx-auto -mt-24">
             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
               <div>
-                <h1 className="text-8xl font-bold text-center">Profile Page</h1>
+                <h1 className="text-6xl md:text-8xl font-bold text-center">Profile Page</h1>
                 <br />
-                <h2 className="text-4xl text-center">
-                  <Link href={`/profile/${data}`}>User Id: {data}</Link>
-                  <br/>
-                  <Link href={`/profile/${username}`}>Username: {username}</Link>
+                <h2 className="text-3xl text-left p-4 py-0">
+                  <div className="leading-relaxed border-2 w-full max-h-screen mx-auto px-auto rounded-lg p-16">
+                    <Link href={`/profile/${data}`}>User Id: {data}</Link>
+                    <br />
+                    <Link href={`/profile/${data}`}>Username: {username}</Link>
+                    <br />
+                    <Link href={`/profile/${data}`}>
+                      User Email: {email}
+                    </Link>
+                  </div>
                 </h2>
               </div>
             </div>
